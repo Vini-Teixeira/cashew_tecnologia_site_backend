0815134829648ec92b540c84ddad4ca6de483506
Vinicius Teixeira <teixeiravinicius32@gmail.com>
2026-06-20 01:09:02 -0300

    reemissão do método de CORS.

--- src/main.ts
+++ src/main.ts
@@ -4,16 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
     app.enableCors({
-    origin: [
-      'http://localhost:4200', 
-      'http://localhost:4201',
-      'https://www.cashewtecnologia.com.br',
-      'https://cashewtecnologia.com.br',
-      'https://moven-track-frotas.vercel.app'
-    ],
+    origin: true, 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
-    allowedHeaders: 'Content-Type, Accept, Authorization, x-api-key',
   });
 
   await app.listen(process.env.PORT ?? 3001);
